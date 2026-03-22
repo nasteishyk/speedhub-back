e8005fbd41233f7b6c03f70d7354e5830ecd35b2
Anastasiia <nastasiia.kletsko@gmail.com>
2026-03-22 15:53:15 +0200

    question swagger v3

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,52 +1,51 @@
 import express from "express";
-import { getAllQuestions, getRandomTest, getQuestionsByUnit } from "../controllers/questionsController.js";
+import {
+  getAllQuestions,
+  getRandomTest,
+  getQuestionsByUnit
+} from "../controllers/questionsController.js";
 
 const router = express.Router();
 
 /**
  * @openapi
- * /api/questions/:
- * get:
- * summary: Отримати всі питання
- * tags: [Questions]
- * responses:
- * 200:
- * description: Список всіх питань
+ * /api/questions:
+ *   get:
+ *     summary: Отримати всі питання
+ *     tags: [Questions]
+ *     responses:
+ *       200:
+ *         description: Успішно
  */
 router.get("/", getAllQuestions);
 
 /**
  * @openapi
  * /api/questions/test:
- * get:
- * summary: Згенерувати випадковий тест
- * tags: [Questions]
- * responses:
- * 200:
- * description: Масив з 20 випадкових питань
+ *   get:
+ *     summary: Випадковий тест
+ *     tags: [Questions]
+ *     responses:
+ *       200:
+ *         description: Успішно
  */
 router.get("/test", getRandomTest);
 
 /**
  * @openapi
  * /api/questions/search-by-unit:
- * get:
- * summary: Пошук питань за номером розділу
- * tags: [Questions]
- * parameters:
- * - in: query
- * name: id
- * required: true
- * schema:
- * type: string
- * description: Номер розділу (наприклад, 1 або r1)
- * responses:
- * 200:
- * description: Успішно знайдено питання
- * 400:
- * description: Не вказано ID
- * 404:
- * description: Питання не знайдені
+ *   get:
+ *     summary: Пошук за розділом
+ *     tags: [Questions]
+ *     parameters:
+ *       - in: query
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Успішно
  */
 router.get("/search-by-unit", getQuestionsByUnit);
 
