7ba3b6d4dc3fc38191980cb1fa7f7a26d7eef61a
Anastasiia <nastasiia.kletsko@gmail.com>
2026-03-29 00:05:43 +0200

    added allowed headers

--- src/server.js
+++ src/server.js
@@ -54,10 +54,9 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma', 'Expires'],
   optionsSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 
 const swaggerOptions = {
