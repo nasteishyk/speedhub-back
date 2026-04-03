065a77d2f41d8b5934fa5cb9dfc2081c20591052
Anastasiia <nastasiia.kletsko@gmail.com>
2026-04-03 12:06:58 +0300

    added exposedHeaders

--- src/server.js
+++ src/server.js
@@ -55,6 +55,7 @@ const corsOptions = {
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma', 'Expires'],
+  exposedHeaders: ['Authorization'],
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
