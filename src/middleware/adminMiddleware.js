88072df5de6fdf517e1edcb4fe3d85b0dd1134f9
Anastasiia <nastasiia.kletsko@gmail.com>
2026-04-19 17:30:35 +0300

    +

--- src/middleware/adminMiddleware.js
+++ src/middleware/adminMiddleware.js
@@ -1,9 +1,6 @@
 export const isAdmin = (req, res, next) => {
   if (req.user && req.user.role === 'admin') {
-    next();
-  } else {
-    res
-      .status(403)
-      .json({ error: 'Доступ заборонено. Тільки для адміністраторів.' });
+    return next();
   }
+  return res.status(403).json({ error: 'Доступ заборонено. Тільки для адміністраторів.' });
 };
